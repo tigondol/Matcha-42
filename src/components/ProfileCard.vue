5818e9b739a356c5a2d39bf59803562fb9f0cc21
tigondol <tigondol@student.42.fr>
2023-05-03 12:31:26 +0200

    matcha 42 first push

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,67 @@
+<template>
+    <q-card class="my-card">
+      <q-img
+        style="cursor: pointer"
+        @click="goToProfile"
+        height="270px"
+        fit="cover"
+        :src="'data:image/png;base64, ' + profile?.pictures[profile.picture_id]?.encrypted_picture">
+        <div class="transparent text-black absolute-top-right">
+          <q-circular-progress
+            size="300%"
+            show-value
+            class="text-positive"
+            color="positive"
+            :value="Math.round(profile.score)"/>
+        </div>
+        <div class="absolute-bottom row">
+          <div margin class="col-auto">
+            <div class="text-h6">
+              {{ profile.first_name }}
+              <q-badge v-if="profile.online" rounded color="positive"/>
+            </div>
+            <div class="text-h9">{{ profile.age }}</div>
+          </div>
+          <div class="col row justify-end">
+            <q-chip v-for="(tag, key) in profile.tags" :key="key" dense size="sm" :label="tag.name"/>
+          </div>
+        </div>
+      </q-img>
+      <q-card-actions class="row wrap justify-between items-start content-start">
+        <q-btn @click="dislike" size="xl" flat round color="negative" icon="close" />
+        <!-- <q-btn size="xl" flat round color="accent" icon="bolt" /> -->
+        <q-btn @click="like" size="xl" flat round color="positive" icon="favorite" />
+      </q-card-actions>
+    </q-card>
+  </template>
+  
+  <style lang="sass" scoped>
+  .my-card
+    width: 270px
+    height: 350px
+    margin: 10px
+  </style>
+  
+  <script>
+//   import { api } from 'boot/axios';
+  
+  export default {
+    name: "ProfileCard",
+    props: [
+      'profile',
+    ],
+    // methods: {
+    //   goToProfile() {
+    //     this.$router.push(`${this.profile.id}`);
+    //   },
+    //   like() {
+    //     this.$emit('update', this.profile.id);
+    //     api.post(`/users/${this.$store.getters['user/getId']}/like/${this.profile.id}`);
+    //   },
+    //   dislike() {
+    //     this.$emit('update', this.profile.id);
+    //     api.post(`/users/${this.$store.getters['user/getId']}/dislike/${this.profile.id}`);
+    //   },
+    // },
+  };
+  </script>
---
+++ src/views/Onboarding.vue
@@ -0,0 +1,183 @@
+<template>
+  <div>
+    <header v-if="$route.meta.title">
+      <Navbar authToken="true" @loginClick="logIn" />
+    </header>
+    <div className="onboarding">
+      <h1>Inscrirption</h1>
+      <form onSubmit="submit">
+        <section>
+          <label for="first_name">Prenom :</label>
+          <input
+            type="text"
+            id="first_name"
+            placeholder="Prenom"
+            required="true"
+            value=""
+            v-on:change="handleChange"
+          />
+
+          <label for="date_of_birth">Date de naissance :</label>
+          <div className="multiple-input-container">
+            <input
+              type="number"
+              id="dob_day"
+              placeholder="DD"
+              required="true"
+              value=""
+              v-on:change="handleChange"
+            />
+            <input
+              type="number"
+              id="dob_month"
+              placeholder="MM"
+              required="true"
+              value=""
+              v-on:change="handleChange"
+            />
+            <input
+              type="number"
+              id="dob_year"
+              placeholder="YYYY"
+              required="true"
+              value=""
+              v-on:change="handleChange"
+            />
+          </div>
+
+          <label>Genre :</label>
+          <div className="multiple-input-container">
+            <input
+              type="radio"
+              id="man-gender-identity"
+              name="gender"
+              required="true"
+              value="male"
+              checked="false"
+              v-on:change="handleChange"
+            />
+            <label>Homme</label>
+            <input
+              type="radio"
+              id="woman-gender-identity"
+              name="gender"
+              required="true"
+              value="female"
+              checked="false"
+              v-on:change="handleChange"
+            />
+            <label>Femme</label>
+            <input
+              type="radio"
+              id="other-gender-identity"
+              name="gender"
+              required="true"
+              value="other"
+              checked="false"
+              v-on:change="handleChange"
+            />
+            <label>Autre</label>
+          </div>
+
+          
+          <label for="show_gender">Afficher mon genre</label>
+          <input
+            type="checkBox"
+            id="show-gender"
+            name="show-gender"
+            required="true"
+            v-on:change="handleChange"
+            checked="false"
+          />
+
+          <label>Montrez moi :</label>
+          <div className="multiple-input-container">
+            <input
+              type="radio"
+              id="man-gender-interest"
+              name="gender_interest"
+              required="true"
+              value="man"
+              checked="false"
+              v-on:change="handleChange"
+            />
+            <label>Homme</label>
+            <input
+              type="radio"
+              id="woman-gender-interest"
+              name="gender_interest"
+              required="true"
+              value="woman"
+              checked="false"
+              v-on:change="handleChange"
+            />
+            <label>Femme</label>
+            <input
+              type="radio"
+              id="all-gender-interest"
+              name="gender_interest"
+              required="true"
+              value="all"
+              checked="false"
+              v-on:change="handleChange"
+            />
+            <label>Tous</label>
+          </div>
+          <label for="show_gender">Description</label>
+          <input
+            type="text"
+            id="description"
+            name="description"
+            required="true"
+            v-on:change="handleChange"
+            checked="false"
+          />
+          <button className="secondary-button" onclick="handleSubmit">Submit</button>
+        </section>
+        <section>
+          <label for="about">Photo de profile</label>
+          <input
+            type="url"
+            name="url"
+            id="url"
+            v-on:change="handleChange">
+          <div></div>
+        </section>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style lang="sass"></style>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+
+export default {
+  name: "Onboarding",
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      authToken: true,
+    };
+  },
+  created() {
+    console.log(this.authToken);
+  },
+  computed: {
+    handleClick() {
+      console.log("clicked");
+    },
+    handleSubmit() {
+      console.log("submitted")
+    },
+    handleChange() {
+      console.log("changed")
+    }
+  },
+  methods: {
+  }
+};
+</script>
